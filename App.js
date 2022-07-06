a6315227bf464c133ac3d384786574ac80c11806
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-07-06 16:51:09 +0100

    Added custom fonts

--- App.js
+++ App.js
@@ -4,7 +4,24 @@ import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 export default function App() {
+  const [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [LatoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!OswaldLoaded || !LatoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
