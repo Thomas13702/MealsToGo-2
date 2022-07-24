c023ba41e21dfccb1e326de66fc95cb150a8cef6
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-07-24 23:17:58 +0100

    SImplified tab icons

--- App.js
+++ App.js
@@ -18,6 +18,12 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
 const MapsScreen = () => {
   return (
     <SafeArea>
@@ -32,6 +38,15 @@ const SettingsScreen = () => {
       <Text>Settings!</Text>
     </SafeArea>
   );
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ color, size }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
 };
 
 export default function App() {
@@ -52,22 +67,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color, size }) => {
-                let iconName;
-
-                if (route.name === "Restaurants") {
-                  iconName = "md-restaurant";
-                } else if (route.name === "Settings") {
-                  iconName = "md-settings";
-                } else if (route.name === "Map") {
-                  iconName = "md-map";
-                }
-
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-            })}
+            screenOptions={createScreenOptions}
             tabBarOptions={{
               activeTintColor: "tomato",
               inactiveTintColor: "gray",
