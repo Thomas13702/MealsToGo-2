df8f8eff902a97c84838786a9fec916a4d170f74
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-08-02 16:58:03 +0100

    Set map region, search takes you to city

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,6 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
+
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
@@ -10,10 +13,35 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.04,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
     </>
   );
 };
