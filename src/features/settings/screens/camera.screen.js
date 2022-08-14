e7cf3aa889a8ac8e47f2d9b032902c15f3d8baa5
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-08-14 22:57:38 +0100

    Added camera/pfp and firebase functons

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,17 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View } from "react-native";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { View, TouchableOpacity } from "react-native";
 import { Camera, CameraType } from "expo-camera";
 import styled from "styled-components";
 import { Text } from "../../../components/typography/text.component";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
-export const CameraScreen = () => {
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const { user } = useContext(AuthenticationContext);
+
+  // console.log(user);
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -27,9 +41,12 @@ export const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <ProfileCamera
-      ref={(camera) => (cameraRef.current = camera)}
-      type={CameraType.front}
-    ></ProfileCamera>
+    <TouchableOpacity onPress={snap}>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        type={CameraType.front}
+        ratio={"16:9"}
+      ></ProfileCamera>
+    </TouchableOpacity>
   );
 };
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 import { List, Avatar } from "react-native-paper";
@@ -8,6 +8,9 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import styled from "styled-components";
 import { TouchableOpacity } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,13 +22,30 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photo = await AsyncStorage.getItem(`${currentUser.user.uid}-photo`);
+    setPhoto(photo);
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
 
   return (
     <SafeArea>
       <List.Section>
         <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
           <AvatarContainer>
-            <Avatar.Icon size={120} icon="human" backgroundColor="#2182BD" />
+            {!photo ? (
+              <Avatar.Icon size={120} icon="human" backgroundColor="#2182BD" />
+            ) : (
+              <Avatar.Image size={120} source={{ uri: photo }} />
+            )}
+
             <Spacer position="top" size="large">
               <Text variant="label">{user.user.email}</Text>
             </Spacer>
