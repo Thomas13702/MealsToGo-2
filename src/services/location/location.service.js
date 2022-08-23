c7c6ad4270e8b23179b94f51c0bb59e039e7334b
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-08-23 23:40:43 +0100

    minor optimizations

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,8 +1,8 @@
 import camelize from "camelize";
-import { host } from "../../utils/env";
+import { host, isMock } from "../../utils/env";
 
 export const locationRequest = async (searchTerm) => {
-  return fetch(`${host}/geocode?city=${searchTerm}`)
+  return fetch(`${host}/geocode?city=${searchTerm}&mock=${isMock}`)
     .then(async (res) => {
       const response = await res.json();
       return response;
--- src/utils/env.js
+++ src/utils/env.js
@@ -7,3 +7,7 @@ const environment = process.env.NODE_ENV || "development";
 export const isDevelopment = environment === "development";
 
 export const host = isDevelopment ? LOCALHOST : LIVEHOST;
+
+// export const host = LIVEHOST;
+
+export const isMock = true;
