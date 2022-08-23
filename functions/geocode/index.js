479e85b97f67f6821df071d3fc35760c8e7784b2
Thomas Pritchard <thomaspritchard137@btinternet.com>
2022-08-23 22:23:01 +0100

    Adding geocoding api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,10 +1,27 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  // console.log(city);
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
+  if (mock == "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
 
-  response.json(locationMock);
+    return response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: "apiKey",
+      },
+      timeout: 1000, // milliseconds
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((err) => {
+      response.status(400);
+      return response.send(err.response.data.error_message);
+    });
 };
