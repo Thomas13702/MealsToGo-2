907eb82a6542e4d845e522b770fedb3b309ee0a5
Thomas Pritchard <thomaspritchard137@btinternet.com>
2023-05-17 12:52:10 +0100

    Commits

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,7 @@
 import { LIVEHOST } from "@env";
 
 const LOCALHOST =
-  "https://c03d-86-137-161-92.ngrok.io/mealstogo-c4b4c/us-central1";
+  "https://4b19-77-98-112-138.ngrok.io/mealstogo-c4b4c/us-central1";
 
 const environment = process.env.NODE_ENV || "development";
 
